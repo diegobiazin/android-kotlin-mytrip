bf4413e2bd875e5943ee1428e98c57c2c2792ec9
Diego Biazin <diego_biazin@hotmail.com>
2018-08-28 22:06:50 -0300

    Implementando validação de dados

--- app/src/main/java/com/diegobiazin/mytrip/MainActivity.kt
+++ app/src/main/java/com/diegobiazin/mytrip/MainActivity.kt
@@ -3,13 +3,14 @@ package com.diegobiazin.mytrip
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         val id = view.id
-        if(id == R.id.buttonCalculate){
+        if (id == R.id.buttonCalculate) {
             handleCalculate()
         }
     }
@@ -18,10 +19,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       buttonCalculate.setOnClickListener(this)
+        buttonCalculate.setOnClickListener(this)
     }
-    
+
     private fun handleCalculate() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (isValid()) {
+
+            try {
+
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(this, getString(R.string.valores_validos), Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.valores_validos), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return editDistance.text.toString() != ""
+                && editPrice.text.toString() != ""
+                && editAutonomy.text.toString() != "0"
     }
 }
