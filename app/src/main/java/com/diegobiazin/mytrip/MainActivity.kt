4b243e8efab521b92bd41452e18e85ae82a5958c
Diego Biazin <diego_biazin@hotmail.com>
2018-08-28 21:22:15 -0300

    Adicionando eventos aos elementos

--- app/src/main/java/com/diegobiazin/mytrip/MainActivity.kt
+++ app/src/main/java/com/diegobiazin/mytrip/MainActivity.kt
@@ -2,12 +2,26 @@ package com.diegobiazin.mytrip
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if(id == R.id.buttonCalculate){
+            handleCalculate()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       buttonCalculate.setOnClickListener(this)
+    }
+    
+    private fun handleCalculate() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
